7e9f2c374b0d0027bc728e1679e7908dc62401fa
John Lafer <jlafer@yahoo.com>
2021-07-06 15:32:56 -0700

    changed /optForSmsSupport param from docName to phoneNum

--- functions/optForSmsSupport.protected.js
+++ functions/optForSmsSupport.protected.js
@@ -4,11 +4,11 @@ exports.handler = async function(context, event, callback) {
     const {body} = event;
     const optionMsg = JSON.parse(body);
     console.log('optForSmsSupport: optionMsg', optionMsg);
-    const {docName, opt} = optionMsg;
+    const {phoneNum, opt} = optionMsg;
     const client = context.getTwilioClient();
     const {SYNC_SERVICE_SID} = context;
     const syncDoc = await client.sync.services(SYNC_SERVICE_SID).documents.create({
-      uniqueName: docName,
+      uniqueName: phoneNum,
       data: {smsSupport: opt},
       ttl: 60
     });
